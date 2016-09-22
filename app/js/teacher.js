7304dc6951d7b927a1c2abb46e2f8778751f3020
Robert Smol <robert.smol@stereoit.com>
2016-09-22 13:52:50 +0200

    Trivial react app skelet

--- app/js/teacher.js
+++ app/js/teacher.js
@@ -1,1 +1,12 @@
-console.log("Teacher app");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+let TeacherApp = () => {
+  return(
+    <div>Hello I am teacher app</div>
+  )
+}
+
+ReactDOM.render(<TeacherApp />, document.getElementById('teacherApp'));
